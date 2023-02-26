96859a47e4f06cb926b5104d7fdb15459b96202b
Creend <creend42@gmail.com>
2023-02-26 17:28:06 +0100

    Add comments api to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../slices/authSlice';
-import { filesApi } from '../services/files';
-import { authApi } from '../services/auth';
-import { usersApi } from '../services/users';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../slices/authSlice";
+import { filesApi } from "../services/files";
+import { authApi } from "../services/auth";
+import { usersApi } from "../services/users";
+import { commentsApi } from "../services/comments";
 
 export const store = configureStore({
   reducer: {
@@ -10,12 +11,14 @@ export const store = configureStore({
     [filesApi.reducerPath]: filesApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
+    [commentsApi.reducerPath]: commentsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       filesApi.middleware,
       authApi.middleware,
-      usersApi.middleware
+      usersApi.middleware,
+      commentsApi.middleware
     ),
   devTools: true,
 });
